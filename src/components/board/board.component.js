c698ce2935494f44128b5160a33902949c43ade4
Yogesh Rajput <86060505+yogesh-06@users.noreply.github.com>
2022-09-26 15:37:42 +0530

    intialised

---
+++ src/components/board/board.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from "@mui/material";
+
+const Board = () => {
+  return (
+    <div>
+      <div>Column 1</div>
+      <div>Column 2</div>
+      <div>Column 3</div>
+      <div>Column 4</div>
+      <div>Column 5</div>
+      <div>Column 6</div>
+    </div>
+  );
+};
+
+export default Board;
