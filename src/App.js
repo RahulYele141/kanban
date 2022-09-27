587609e2eae48df3f69b50240e4690d1039d7a91
RahulYele141 <raytheperfect@gmail.com>
2022-09-27 15:19:49 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Board from './components/board/board.component';
+import Sidebar from './components/sidebar/sidebar.component';
 
 
 function App() {
   return (
     <div className="App">
-      <Board />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/sidebar/sidebar.component.jsx
@@ -0,0 +1,46 @@
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+import React from "react";
+
+const Sidebar = () => {
+    console.log('sidebar');
+    return (
+        <div>
+            <Drawer variant="temporary"
+                ModalProps={{
+                    keepMounted: true,
+                }} >
+
+                <div>
+                    <Toolbar />
+                    <Divider />
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </Drawer>
+        </div>)
+}
+
+export default Sidebar
